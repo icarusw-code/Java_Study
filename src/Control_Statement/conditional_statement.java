162b5e232171e2179d8e7b6f32be0ec02897eeed
icarusw-code <77170611+icarusw-code@users.noreply.github.com>
2021-12-10 22:56:54 +0900

    first commit

---
+++ src/Control_Statement/conditional_statement.java
@@ -0,0 +1,18 @@
+package Control_Statement;
+
+public class conditional_statement {
+    public static void main(String[] args){
+        System.out.println("a");
+        if(false){
+            System.out.println(1);
+        }
+        else if(true){
+            System.out.println(2);
+        }
+        else{
+            System.out.println(3);
+        }
+        System.out.println("b");
+    }
+
+}
---
+++ src/Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype {
+    public static void main(String[] args){
+        System.out.println(7); // Number
+        System.out.println("Seven"); // String
+
+        System.out.println("7"); // String
+        System.out.println(6+6); // 12
+        System.out.println("6" + "6"); // 66
+
+        System.out.println(6*6); // 36
+//      System.out.println("6" * "6"); // 연산 불가
+
+        System.out.println("1111".length()); // 4
+//        System.out.println(1111.length()); // 연산 불가
+    }
+}
